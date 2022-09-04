59f74b64f9d23d24d09a66e5c62b82c4631d7818
SeunPaul <seunpaul148@gmail.com>
2022-09-04 22:42:04 +0100

    db setup for heroku

--- config/db.js
+++ config/db.js
@@ -2,19 +2,12 @@ const knex = require("knex");
 
 const db = knex({
   client: "pg",
-  connection:
-    // {
-    //   connectionString: process.env.DATABASE_URL,
-    //   ssl: {
-    //     rejectUnauthorized: false
-    //   }
-    // }
-    {
-      host: "127.0.0.1",
-      database: "agfst",
-      user: "seun",
-      password: "0030335"
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
     }
+  }
 });
 
 module.exports = db;
